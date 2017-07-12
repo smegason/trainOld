4a4bde1c9bc90df223473803e1fc7ef761634e41
smegason <megason@hms.harvard.edu>
2017-07-11 22:57:39 -0400

    add php

---
+++ www/php/newUser.php
@@ -0,0 +1,40 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table {
+    width: 100%;
+    border-collapse: collapse;
+}
+
+table, td, th {
+    border: 1px solid black;
+    padding: 5px;
+}
+
+th {text-align: left;}
+</style>
+</head>
+<body>
+New user----
+<?php
+$username = htmlspecialchars($_GET["username"]);
+$email = htmlspecialchars($_GET['email']);
+$password = htmlspecialchars($_GET['password']);
+echo "<br>username=".$username.", pass=".$password.", email=".$email;
+
+$con = mysqli_connect('localhost','root','X*rgXCY_#7l7','trainDB');
+if (!$con) {
+    die('Could not connect: ' . mysqli_error($con));
+}
+
+$sql = "INSERT INTO users (userName, password, email) VALUES ('".$username."', '".$password."', '".$email."')";
+echo "<br>sql=".$sql;
+$result = mysqli_query($con,$sql);
+echo "<br>result=".$result;
+
+mysqli_close($con);
+?>
+</body>
+</html>
---
+++ www/php/signinUser.php
@@ -0,0 +1,22 @@
+
+<?php
+$username = htmlspecialchars($_GET["username"]);
+$password = htmlspecialchars($_GET['password']);
+
+$con = mysqli_connect('localhost','root','X*rgXCY_#7l7','trainDB');
+if (!$con) {
+    die('fail-connect');
+}
+
+$sql = "SELECT * FROM users WHERE userName='".$username."' AND password='".$password."'";
+$result = mysqli_query($con,$sql);
+if ($result) {
+	$row = mysqli_fetch_array($result);
+	$userID = $row['userID'];
+	$username = $row['userName'];
+	echo "&&&".$userID."&&&".$username."&&&";
+} else {
+	echo "fail-login";
+}
+mysqli_close($con);
+?>
