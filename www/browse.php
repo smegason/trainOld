d894e9788c0f7263169ccc8ee0e6d9472b860c0b
smegason <megason@hms.harvard.edu>
2017-07-18 10:55:47 -0400

    zz

--- www/browse.php
+++ www/browse.php
@@ -46,6 +46,7 @@ Cache-Control:public;
 				echo "<h2>".$trackName."</h2>";
 				echo "<h3> by ".$trainusername."</h3>";
 				echo "<p>".$trackDescription."</p>";
+				echo "<p>encTrx=".$encodedTrx."</p>";
 				echo "</td><td>";
 
 				echo '<td class="style24" style="width: 400px">';
